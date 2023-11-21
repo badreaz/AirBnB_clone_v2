25ca58e19e694eb480f7b1ed73b1790b945cbd01
badreaz <baz.23.2019@gmail.com>
2023-11-21 18:33:04 +0400

    update class attribute

--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from models.storage import storage
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    @property
+    def cities(self):
+        """ returns a list of cities """
+        city = []
+        for ins in storage.all(City).values():
+            if self.id == ins.state_id:
+                city.append(ins)
+        return city
