728f3bcc5fdec74f2808651eff1c2846281b74d0
badreaz <baz.23.2019@gmail.com>
2023-11-21 21:47:42 +0400

    add many-to-many relationship with amenity

--- models/place.py
+++ models/place.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import models
 from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from models.review import Review
+from models.amenity import Amenity
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+
+metadata = Base.metadata
+place_amenity = Table("place_amenity", metadata,
+        Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False)
+        Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False)
+        )
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -19,13 +29,32 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
     reviews = relationship("Reivew", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
 
     @property
     def reviews(self):
         """ returns the list of Review instances with place_id
         equals to the current place.id """
         review = []
-        for ins in storage.all(Review).values():
+        for ins in models.storage.all(Review).values():
             if self.id == ins.place_id:
                 review.append(ins)
         return review
+
+    @property
+    def amenities(self):
+        """ returns the list of Amenity instances based on
+        an attribute amenity_ids that contains all Amenity.id
+        linked to the place """
+        amenity = []
+        for ins in models.storage.all(Amenity).values():
+            if ins.id in self.amenity_ids:
+                amenity.append(ins)
+        return amenity
+
+    @amenities.setter
+    def amenities(self, obj):
+        """ handles append method for adding an Amenity.id
+        to the attribute amenity_ids """
+        if type(obj) == Amenity:
+            self.amenity_ids.append(obj.id)
