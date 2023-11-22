784e476a8a692224592c85ae30585a60c2ca1d52
badreaz <baz.23.2019@gmail.com>
2023-11-22 19:10:32 +0400

    fix syntax error

--- models/base_model.py
+++ models/base_model.py
@@ -19,9 +19,9 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if Kwargs:
+        if kwargs:
             for k, v in kwargs.items():
-                if k == "createdd_at" or k == "updated_at":
+                if k == "created_at" or k == "updated_at":
                     v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
                 if k != "__class__":
                     setattr(self, k, v)
