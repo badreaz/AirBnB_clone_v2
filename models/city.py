43d44e3a065bba07688ba9cadf84c52a70a26daa
badreaz <baz.23.2019@gmail.com>
2023-11-22 22:29:56 +0400

    check storage type

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+from os import getenv
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
@@ -10,4 +11,5 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", backref="cities", cascade="delete")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        places = relationship("Place", backref="cities", cascade="delete")
