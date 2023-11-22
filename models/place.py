df4796400938d4f68fa0923a87af89b16891f20c
badreaz <baz.23.2019@gmail.com>
2023-11-22 21:19:21 +0400

    fix errors

--- models/place.py
+++ models/place.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 import models
+from os import getenv
 from models.base_model import Base, BaseModel
 from models.review import Review
 from models.amenity import Amenity
@@ -20,41 +21,42 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
     reviews = relationship("Reivew", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
 
-    @property
-    def reviews(self):
-        """ returns the list of Review instances with place_id
-        equals to the current place.id """
-        review = []
-        for ins in models.storage.all(Review).values():
-            if self.id == ins.place_id:
-                review.append(ins)
-        return review
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def reviews(self):
+            """ returns the list of Review instances with place_id
+            equals to the current place.id """
+            review = []
+            for ins in models.storage.all(Review).values():
+                if self.id == ins.place_id:
+                    review.append(ins)
+            return review
 
-    @property
-    def amenities(self):
-        """ returns the list of Amenity instances based on
-        an attribute amenity_ids that contains all Amenity.id
-        linked to the place """
-        amenity = []
-        for ins in models.storage.all(Amenity).values():
-            if ins.id in self.amenity_ids:
-                amenity.append(ins)
-        return amenity
+        @property
+        def amenities(self):
+            """ returns the list of Amenity instances based on
+            an attribute amenity_ids that contains all Amenity.id
+            linked to the place """
+            amenity = []
+            for ins in models.storage.all(Amenity).values():
+                if ins.id in self.amenity_ids:
+                    amenity.append(ins)
+            return amenity
 
-    @amenities.setter
-    def amenities(self, obj):
-        """ handles append method for adding an Amenity.id
-        to the attribute amenity_ids """
-        if type(obj) == Amenity:
-            self.amenity_ids.append(obj.id)
+        @amenities.setter
+        def amenities(self, obj):
+            """ handles append method for adding an Amenity.id
+            to the attribute amenity_ids """
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import models
+from os import getenv
 from models.base_model import Base, BaseModel
 from models.city import City
 from sqlalchemy import Column, String
@@ -13,11 +14,12 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
 
-    @property
-    def cities(self):
-        """ returns a list of cities """
-        city = []
-        for ins in models.storage.all(City).values():
-            if self.id == ins.state_id:
-                city.append(ins)
-        return city
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """ returns a list of cities """
+            city = []
+            for ins in models.storage.all(City).values():
+                if self.id == ins.state_id:
+                    city.append(ins)
+            return city
