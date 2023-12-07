b46d90237489cdd586ae14443c8424da214e017b
badreaz <baz.23.2019@gmail.com>
2023-12-07 20:41:53 +0400

    edit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,6 +4,7 @@ import os
 from datetime import datetime
 from fabric.api import env, local, put, run
 
+
 def do_pack():
     """ generates a .tgz archive from the contents
     of the web_static folder """
@@ -17,6 +18,7 @@ def do_pack():
         return None
     return archive
 
+
 env.hosts = ['100.25.33.231', '54.237.87.188']
 
 
@@ -28,20 +30,23 @@ def do_deploy(archive_path):
         file = archive_path.split("/")[-1]
         file_name = file.split(".")[0]
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(file_name))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        run("rm -rf /data/web_static/releases/{}/".format(file_name))
+        run("mkdir -p /data/web_static/releases/{}/".format(file_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(file, file_name))
-        run("sudo rm /tmp/{}".format(file))
-        run("sudo mv /data/web_static/releases/{}/web_static/* "
+        run("rm /tmp/{}".format(file))
+        run("mv /data/web_static/releases/{}/web_static/* "
             "/data/web_static/releases/{}/".format(file_name, file_name))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(file_name))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(file_name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(file_name))
         return True
     except Exception:
         return False
-    
+
+
 def deploy():
     """  create and distribute an archive to a web server """
     return do_deploy(do_pack())
