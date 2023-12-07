6bf5bcc59a01f18292838587f51481385e666000
badreaz <baz.23.2019@gmail.com>
2023-12-07 19:44:00 +0400

    edit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,19 +11,19 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        file = archive_path.split("/")[-1].split(".")[0]
+        file = archive_path.split("/")[-1]
+        file_name = file.split(".")[0]
         put(archive_path, "/tmp/")
-        run("rm -rf /data/web_static/releases/{}/".format(file))
-        run("mkdir -p /data/web_static/releases/{}/".format(file))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            file + ".tgz", file))
-        run("rm /tmp/{}".format(file + ".tgz"))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(file, file))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(file))
+        run("mkdir -p /data/web_static/releases/{}/".format(file_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(file, file_name))
+        run("rm /tmp/{}".format(file))
+        run("mv /data/web_static/releases/{}/web_static/*"
+            " /data/web_static/releases/{}/".format(file_name, file_name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(file_name))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ \
-            /data/web_static/releases/{}/".format(file))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/releases/{}/"
+            .format(file_name))
         return True
     except Exception:
         return False
